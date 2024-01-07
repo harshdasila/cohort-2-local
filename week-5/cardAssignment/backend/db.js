c510bdee2dda8ffdee082cd3997fce41725aa8af
Harsh Dasila <74102378+harshdasila@users.noreply.github.com>
2024-01-07 16:01:01 +0530

    Removed mongo url

--- week-5/cardAssignment/backend/db.js
+++ week-5/cardAssignment/backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://harshdasila5555:Harsh%406112@cluster0.muz5vq8.mongodb.net/Card')
+mongoose.connect('ur-mongo-url')
 
 const CardSchema = mongoose.Schema({
     fName: String,
